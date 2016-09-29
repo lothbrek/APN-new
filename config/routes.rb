1c525d3861858a5396f39ef9910d1269971104cd
MattSMcCullar <mattaym@gmail.com>
2016-09-28 23:38:38 -0500

    Begin posts and simple CRUD operations.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+	resources :posts
+
   root 'pages#home'
 
   get '/about', to: 'pages#about'
 
   get '/contact', to: 'pages#contact'
 
+  get '/blog', to: 'posts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
